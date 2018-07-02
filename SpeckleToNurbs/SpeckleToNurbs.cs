a048d391e6e422a69ef0ae5c44cd1e0fb522465a
AURECON\Chris.Welch <chris.welch@aurecongroup.com>
2018-07-03 08:55:42 +1200

    initial setup

---
+++ SpeckleToNurbs/SpeckleToNurbs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeckleCore;
+using UnityNurbs;
+
+public static class SpeckleToNurbs {
+
+        public static Vector3 ToVector3(this SpecklePoint p)
+        {
+            return new Vector3((float)p.Value[0], (float)p.Value[1], (float)p.Value[2]);
+        }
+
+        public static Vector3 ToVector3(this SpeckleVector p)
+        {
+            return new Vector3((float)p.Value[0], (float)p.Value[1], (float)p.Value[2]);
+        }
+
+        public static Arc ToVerb(this SpeckleArc sArc)
+        {
+            var plane = sArc.Plane;
+
+            return new Arc(plane.Origin.ToVector3(), plane.Xdir.ToVector3(), plane.Ydir.ToVector3(), (float)sArc.Radius, (float)sArc.StartAngle, (float)sArc.EndAngle);
+        }
+    
+
+
+
+}
